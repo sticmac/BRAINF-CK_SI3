6592425ac4003ac52f14ec031da59a82ac78f293
Sticmac <jujulem06@hotmail.fr>
2016-09-28 14:47:38 +0200

    bfck script reads arguments and pass them to java program

--- src/brainfuck/Main.java
+++ src/brainfuck/Main.java
@@ -5,7 +5,6 @@ import brainfuck.virtualmachine.OverflowException;
 
 public class Main {
 	public static void main(String[] args) throws OverflowException {
-		//InstructionList ilist = new InstructionList();
 		Machine machine = new Machine();
 		machine.executeOp("INCR");
 		System.out.println(machine.dumpMemory());
