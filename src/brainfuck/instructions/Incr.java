e5e839931a5c8ad0213e18ef0fc1dcd49e6aa0f2
piernov <piernov@piernov.org>
2016-09-29 19:53:38 +0200

    Add JavaDoc to Instruction, InstructionSet, Decr, Incr, Left, Right, Machine, Memory

--- src/brainfuck/instructions/Incr.java
+++ src/brainfuck/instructions/Incr.java
@@ -4,11 +4,29 @@ import brainfuck.Instruction;
 import brainfuck.virtualmachine.Machine;
 import brainfuck.virtualmachine.OverflowException;
 
+/**
+ * Incr instruction: increments the current memory cell by one.
+ *
+ * @author Pierre-Emmanuel Novac
+ * @see Instruction
+ * @see Machine
+ * @see brainfuck.virtualmachine.Memory
+ */
 public class Incr extends Instruction {
+	/**
+	 * Constructs the Incr instruction.
+	 */
 	public Incr() {
 		super("INCR", '+', "blue"); // FIXME
 	}
 
+	/**
+	 * Action performed by the instruction: increment the current memory cell by one.
+	 * Overrides <a href="https://docs.oracle.com/javase/8/docs/api/java/util/function/Consumer.html">Consumer</a>'s method.
+	 *
+	 * @param machine	Virtual Machine whose state will be altered
+	 * @throws OverflowException	if the current cell value is at it's top limit.
+	 */
 	@Override
 	public void accept(Machine machine) throws OverflowException {
 		byte value = machine.readMemory();
