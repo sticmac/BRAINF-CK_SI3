94c16bfcf5b3b4118248401c24153233adf53bcd
piernov <piernov@piernov.org>
2016-09-29 19:53:55 +0200

    Build JavaDoc with build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # build script
 
-javac -d bin/ -cp bin/ `find src/brainfuck -name \*.java`
-cd bin/
+# Build classes
+javac -Xlint:unchecked -d bin/ -cp bin/ `find src/brainfuck -name \*.java`
+
+# Builds the jar archive
 # See https://docs.oracle.com/javase/tutorial/deployment/jar/appman.html
+cd bin
 jar vcfe ../Bfck.jar brainfuck.Main brainfuck/
 cd ..
+
+# Builds the JavaDoc
+javadoc -d doc -sourcepath src -subpackages brainfuck -private
