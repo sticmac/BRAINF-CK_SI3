e0ed8376d10940301efbe60686ab7dbef680acae
Julien Lemaire <jujulem06@hotmail.fr>
2016-09-30 15:47:28 +0200

    Added javadoc to ArgParser

--- src/brainfuck/Main.java
+++ src/brainfuck/Main.java
@@ -5,13 +5,17 @@ import brainfuck.virtualmachine.OverflowException;
 
 public class Main {
 	public static void main(String[] args) throws OverflowException {
-		//InstructionList ilist = new InstructionList();
 		Machine machine = new Machine();
-		ArgParser parser = new ArgParser(args);
 
 		machine.executeOp("INCR");
 		System.out.println(machine.dumpMemory());
+		
 		machine.executeOp('-');
+		System.out.println(machine.dumpMemory());
+
+		for (int i = 0; i < 255; i++) {
+			machine.executeOp("INCR");
+		}
 		System.out.println(machine.dumpMemory());
 	}
 }
