fd3246bca001e21d3db250c4c00ffb78bbf8466d
Sticmac <jujulem06@hotmail.fr>
2016-09-30 15:24:35 +0200

    Added Javadoc to ArgParser and SyntaxException

--- src/brainfuck/Main.java
+++ src/brainfuck/Main.java
@@ -5,7 +5,6 @@ import brainfuck.virtualmachine.OverflowException;
 
 public class Main {
 	public static void main(String[] args) throws OverflowException {
-
 		Machine machine = new Machine();
 
 		machine.executeOp("INCR");
