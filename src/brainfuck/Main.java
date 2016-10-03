93bef20ba28b274c096f8cc809a775d7796214c0
piernov <piernov@piernov.org>
2016-10-03 11:20:50 +0200

    Adjust the Main accordingly

--- src/brainfuck/Main.java
+++ src/brainfuck/Main.java
@@ -1,23 +1,18 @@
 package brainfuck;
 
-import java.util.Arrays;
+import java.io.IOException;
 import brainfuck.virtualmachine.Machine;
 
 public class Main {
-	public static void main(String[] args) throws SyntaxException {
-		ArgParser parser = new ArgParser(args); 
+	public static void main(String[] args) throws SyntaxException, IOException {
+		ArgParser argp = new ArgParser(args);
+
+		ReadFile file = new ReadFile(argp.getFilename());
 
 		Machine machine = new Machine();
 
-		machine.executeOp("INCR");
-		System.out.println(machine.dumpMemory());
+		Interpreter interpreter = new Interpreter(file.getLines());
 
-		machine.executeOp('-');
-		System.out.println(machine.dumpMemory());
-
-		for (int i = 0; i < 255; i++) {
-			machine.executeOp("INCR");
-		}
-		System.out.println(machine.dumpMemory());
+		interpreter.run(machine);
 	}
 }
