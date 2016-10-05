f3eddc20c3d156ce0fcf399202d5ff175ee01ee0
Tordesillas <bubble83440@gmail.com>
2016-10-05 15:51:46 +0200

    Translator added

---
+++ src/brainfuck/Translator.java
@@ -0,0 +1,36 @@
+package brainfuck;
+
+/**
+ * Translate an instruction in an other form.
+ * @author Miaou
+ * @see Instruction
+ */
+
+ public class Translator {
+   /**
+ 	 * Write the instruction with the long syntax.
+   *
+   * @param instr	instruction to translate.
+ 	 */
+   public void toLongSyntax(Instruction instr) {
+     System.out.print(instr.getName());
+   }
+
+   /**
+ 	 * Write the instruction in the short syntax.
+ 	 *
+ 	 * @param instr	instruction to translate.
+ 	 */
+   public void toShortSyntax(Instruction instr) {
+     System.out.print(instr.getSymbol());
+   }
+
+   /**
+ 	 * Write the instruction in a sequence of hexadecimal numbers.
+ 	 *
+ 	 * @param instr	instruction to translate.
+ 	 */
+   public void toColor(Instruction instr) {
+     System.out.print(String.format("%02X%02X%02X", instr.getColor()[0], instr.getColor()[1], instr.getColor()[2]));
+   }
+ }
