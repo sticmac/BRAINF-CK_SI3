93f0dc29e5569aea4ca8861595045ed2d6a61859
Sticmac <jujulem06@hotmail.fr>
2016-10-07 16:38:49 +0200

    Added javadoc to ReadImage

--- src/brainfuck/ReadImage.java
+++ src/brainfuck/ReadImage.java
@@ -11,10 +11,20 @@ import java.util.stream.IntStream;
 
 import brainfuck.virtualmachine.Machine;
 
+/**
+ * Reading an image and extract color informations for each 3x3 square to be converted into matching instructions in the future.
+ * The parsed image MUST be in .bmp format.
+ * @author MIAOU
+ */
 public class ReadImage {
 	private BufferedImage img;
 	private List<Integer> instructions;
 
+	/**
+	 * Main constructor of the <code>ReadImage</code> class.
+	 * @param url The url of the parsed image.
+	 * @throws IOException if the given URL doesn't exist, or if reading failed.
+	 */
 	public ReadImage(String url) throws IOException {
 		img = ImageIO.read(new File(url));
 		instructions = new ArrayList<>();
@@ -29,6 +39,10 @@ public class ReadImage {
 		}
 	}
 
+	/**
+	 * Returns the colors of the parsed image, as an <code>IntStream</code> containing integers matching the RGB values of each square.
+	 * @return an <code>IntStream</code> containing the integer value of each color.
+	 */
 	public IntStream getInstructions() {
 		return instructions.stream().mapToInt(Integer::intValue);
 	}
