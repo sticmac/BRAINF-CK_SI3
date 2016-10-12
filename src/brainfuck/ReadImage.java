cdbc5342a01123cb2abc2167fecbc12cb478e219
piernov <piernov@piernov.org>
2016-10-12 20:27:06 +0200

    instructions → colors in ReadImage

--- src/brainfuck/ReadImage.java
+++ src/brainfuck/ReadImage.java
@@ -9,41 +9,42 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.stream.IntStream;
 
-import brainfuck.virtualmachine.Machine;
-
 /**
  * Reading an image and extract color informations for each 3x3 square to be converted into matching instructions in the future.
  * The parsed image MUST be in .bmp format.
- * @author MIAOU
+ *
+ * @author Julien Lemaire
  */
 public class ReadImage {
 	private BufferedImage img;
-	private List<Integer> instructions;
+	private List<Integer> colors;
 
 	/**
 	 * Main constructor of the <code>ReadImage</code> class.
+	 *
 	 * @param url The url of the parsed image.
 	 * @throws IOException if the given URL doesn't exist, or if reading failed.
 	 */
 	public ReadImage(String url) throws IOException {
 		img = ImageIO.read(new File(url));
-		instructions = new ArrayList<>();
+		colors = new ArrayList<>();
 
 		int height = img.getHeight();
 		int width = img.getWidth();
 
 		for (int y = 0 ; y < height ; y+=3) {
 			for (int x = 0 ; x < width ; x+=3) {
-				instructions.add(img.getRGB(x, y));
+				colors.add(img.getRGB(x, y));
 			}
 		}
 	}
 
 	/**
 	 * Returns the colors of the parsed image, as an <code>IntStream</code> containing integers matching the RGB values of each square.
+	 *
 	 * @return an <code>IntStream</code> containing the integer value of each color.
 	 */
 	public IntStream getData() {
-		return instructions.stream().mapToInt(Integer::intValue);
+		return colors.stream().mapToInt(Integer::intValue);
 	}
 }
