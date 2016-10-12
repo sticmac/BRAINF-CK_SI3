e45dc5d6ef5d05a45b5238ccf7d9602aa18b795a
piernov <piernov@piernov.org>
2016-10-12 20:27:31 +0200

    Small formatting cleanup on ArgParser

--- src/brainfuck/ArgParser.java
+++ src/brainfuck/ArgParser.java
@@ -4,19 +4,19 @@ package brainfuck;
  * Parser for the arguments passed through the call of the bfck executable.
  * It also stores the different parsed arguments into fields relative to their function.
  * e.g : if a filename is passed through bfck command, it is stored as a filename in this class.
+ *
  * @author Julien Lemaire
  */
 public class ArgParser {
-	//fields
 	private String filename = "";
 	private String in;
 	private String out;
 	private Mode mode;
 
-	//methods
 	/**
 	 * Main constructor of the <code>ArgParser</code> class.
 	 * It takes an array of String, respresenting the arguments, and parse it to store them.
+ 	 *
 	 * @param args Array of String containing all the arguments passed through the executable.
 	 */
 	public ArgParser(String[] args) throws SyntaxException {
@@ -68,6 +68,7 @@ public class ArgParser {
 
 	/**
 	 * Getter for the filename, if any.
+	 *
 	 * @return The filename if one was passed, else an empty string.
 	 */
 	public String getFilename() {
@@ -75,23 +76,26 @@ public class ArgParser {
 	}
 
 	/**
-	 * Getter for the name of the input file. 
+	 * Getter for the name of the input file.
+	 *
 	 * @return The name of the input file if one was passed, else null.
 	 */
 	public String getInput() {
 		return in;
 	}
-	
+
 	/**
-	 * Getter for the name of the output file. 
+	 * Getter for the name of the output file.
+	 *
 	 * @return The name of the output file if one was passed, else null.
 	 */
 	public String getOutput() {
 		return out;
 	}
-	
+
 	/**
-	 * Getter for the mode of execution. 
+	 * Getter for the mode of execution.
+	 *
 	 * @return The program's mode of execution (by default "READFILE").
 	 */
 	public Mode getMode() {
@@ -101,6 +105,7 @@ public class ArgParser {
 
 /**
  * The mode of execution of the current instance of the program.
+ *
  * @author Julien Lemaire
  */
 enum Mode {
@@ -109,8 +114,9 @@ enum Mode {
 
 /**
  * An exception thrown when arguments passed through the bfck executable are not correct.
+ *
  * @author Julien Lemaire
- */ 
+ */
 class SyntaxException extends RuntimeException {
 	/**
 	 * The main constructor of the <code>SyntaxException</code> class.
