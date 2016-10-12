5c50ee3f62b572246f35f1c4df6215999a3ed2d5
piernov <piernov@piernov.org>
2016-10-12 20:30:15 +0200

    Javadoc in BracketCounter and Checker

--- src/brainfuck/Checker.java
+++ src/brainfuck/Checker.java
@@ -3,10 +3,29 @@ package brainfuck;
 import java.util.List;
 import brainfuck.instructions.ConditionalJump;
 
+/**
+ * Checks whether the program is well formed or not.
+ * A well formed program has got a matching Back instruction for every Jump instruction.
+ *
+ * @author Pierre-Emmanuel Novac
+ */
 public class Checker {
+	/**
+	 * List of instructions which will be checked.
+	 */
 	private List<Instruction> instructions;
+
+	/**
+	 * Bracket counter for counting Jump and Back instructions (respectively called right bracket and left bracket).
+	 */
 	private BracketCounter bracketCounter;
 
+	/**
+	 * Constructs a new Checker using the given list of instructions.
+	 * Gives an onMismatch() callback to the bracket counter which exits with error code 4.
+	 *
+	 * @param instructions	List of Instruction on which the check should be performed.
+	 */
 	public Checker(List<Instruction> instructions) {
 		this.instructions = instructions;
 		bracketCounter = new BracketCounter() {
@@ -17,12 +36,15 @@ public class Checker {
 		};
 	}
 
+	/**
+	 * Performs the check.
+	 */
 	public void check() {
 		instructions.forEach(i -> {
-			if (i instanceof ConditionalJump) {
-				((ConditionalJump) i).incr(bracketCounter);
+			if (i instanceof ConditionalJump) { // Checks if the Instruction is either a Jump or a Back, both of which implements ConditionalJump
+				((ConditionalJump) i).incr(bracketCounter); // Increment either the right of left bracket counter
 			}
 		});
-		bracketCounter.checkMatching();
+		bracketCounter.checkMatching(); // Finally check if all the Jump instructions has got a Back instruction.
 	}
 }
