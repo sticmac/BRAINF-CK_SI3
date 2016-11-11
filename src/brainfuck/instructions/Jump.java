d78c9cd70cc322c0ef7ae5b39e417c3baac1ecc9
Julien Lemaire <julien.lemaire@etu.unice.fr>
2016-11-11 14:13:34 +0100

    Better organization of Instruction classes (matching their behaviour with Metrics)

--- src/brainfuck/instructions/Jump.java
+++ src/brainfuck/instructions/Jump.java
@@ -30,13 +30,13 @@ public class Jump extends ConditionalJump {
 	 */
 	@Override
 	public void accept(Machine machine) {
+		super.accept(machine);
 		if (!machine.isJumping() && machine.readMemory() == Byte.MIN_VALUE) {
 			machine.setJumping(true);
 		} else {
 			machine.setReversed(false);
 			machine.setJumping(false);
 		}
-                Metrics.DATA_READ.incr();
 	}
 
 	/**
