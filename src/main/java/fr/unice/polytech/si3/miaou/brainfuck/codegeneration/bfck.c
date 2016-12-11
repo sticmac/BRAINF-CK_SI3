55a762813b7f38e3c5a4b04d728b52a6973086da
Guillaume Casagrande <guillaume.casagrande@etu.unice.fr>
2016-12-11 13:49:53 +0100

    File in C now works

--- src/main/java/fr/unice/polytech/si3/miaou/brainfuck/codegeneration/bfck.c
+++ src/main/java/fr/unice/polytech/si3/miaou/brainfuck/codegeneration/bfck.c
@@ -1,59 +1,145 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "stack.h"
-#include "listes.h"
+#include <string.h>
 
 #define SIZE_MEMORY 300/*00*/
 #define OFFSET 128
 
 #define INCR 	memory[i]++;
 #define DECR 	memory[i]--;
-#define BACK 	if (memory[i] != 0) { j = pop_item(&s); }
+#define BACK 	if (memory[i] != 0) { j = pop_item(&s); p = getElement(l, j); }
 #define IN 		getchar();
 #define JUMP 	push_item(&s, j);
 #define OUT 	printf("%c\n", memory[i]);
 #define RIGHT 	i++;
 #define LEFT 	i--;
 
+
+//Pile
+struct elt {
+	int value;
+	struct elt *next;
+};
+
+typedef struct elt elt;
+typedef elt *Stack;
+
+void push_item(Stack *stack, int n) {
+	elt *new = malloc(sizeof(elt));
+	new->value = n;
+	new->next = *stack;
+	*stack = new;
+}
+
+int pop_item(Stack *stack) {
+	elt *old = *stack;
+
+	if (old) {
+    	int old_value = old->value;
+    	*stack = (*stack)->next;
+    	free(old);
+    	return old_value;
+	}
+	exit(1);
+}
+
+
+//Liste
+struct element {
+	char instr;
+	struct element *next;
+};
+
+typedef struct element Element;
+typedef Element *List;
+
+List append_element(List lst, char s) {
+	Element *tmp = malloc(sizeof(Element));
+	tmp->instr = s;
+	tmp->next  = NULL;
+
+	if (lst == NULL) { return tmp; }
+
+	Element *p;
+	for (p=lst; p->next; p = p->next) {}
+
+	p->next = tmp;
+	return lst;
+}
+
+Element *getElement(List lst, int count) {
+	Element *p = lst;
+	for (int k = 0; k < count; k++) {
+		p = p->next;
+	}
+	return p;
+}
+
+
 int main() {
 	char memory[SIZE_MEMORY] = {};	 //mémoire
 	int i = 0;						 //pointeur en mémoire
 	int j = 0;	 					 //pointeur d'instruction
+	int nbOfInstructions = 0;		 //nombre d'instructions
 	Stack s = NULL;					 //pile de jump
 	List l = NULL;					 //liste d'instructions
 
-	l = append_element(l, "INCR");
-	j++;
+	l = append_element(l, '+');
+	nbOfInstructions++;
 
-	l = append_element(l, "INCR");
-	j++;
+	l = append_element(l, '+');
+	nbOfInstructions++;
 
-	l = append_element(l, "JUMP");
-	j++;
+	l = append_element(l, '[');
+	nbOfInstructions++;
 
-	l = append_element(l, "RIGHT");
-	j++;
+	l = append_element(l, '>');
+	nbOfInstructions++;
 
-	l = append_element(l, "INCR");
-	j++;
+	l = append_element(l, '+');
+	nbOfInstructions++;
 
-	l = append_element(l, "INCR");
-	j++;
+	l = append_element(l, '+');
+	nbOfInstructions++;
 
-	l = append_element(l, "LEFT");
-	j++;
+	l = append_element(l, '<');
+	nbOfInstructions++;
 
-	l = append_element(l, "DECR");
-	j++;
+	l = append_element(l, '-');
+	nbOfInstructions++;
 
-	l = append_element(l, "BACK");
-	j++;
+	l = append_element(l, ']');
+	nbOfInstructions++;
 
-
-	j = 0;
-	for (Element *p = l; p; p = p->next) {
-		j++;
-		printf("%s\n", p-> value);
+	Element *p = l;
+	for (j = 0; j < nbOfInstructions; j++) {
+		switch (p->instr) {
+			case '+':
+				INCR
+				break;
+			case '-':
+				DECR
+				break;
+			case ',':
+				IN
+				break;
+			case '.':
+				OUT
+				break;
+			case '<':
+				LEFT
+				break;
+			case '>':
+				RIGHT
+				break;
+			case '[':
+				JUMP
+				break;
+			case ']':
+				BACK
+				break;
+		}
+		p = p->next;
     }
 
 	//dump
--- src/main/java/fr/unice/polytech/si3/miaou/brainfuck/codegeneration/stack.h
+++
@@ -1,8 +0,0 @@
-typedef struct element Element;
-typedef Element *Stack;
-
-void push_item(Stack *stack, int n);
-
-int pop_item(Stack *stack);
-
-void print_stack(Stack stack);
